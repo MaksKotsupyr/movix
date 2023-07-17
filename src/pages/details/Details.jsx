37923665d7208d4ef9c0fc0d77a107af4b684356
KOTSUPYR MAKSYM <maksym.kotsupyr@gmail.com>
2023-07-17 15:57:12 +0300

    Details banner section - banner, play btn, youtube popup, general information about the film

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './style.scss'
+import { useParams } from 'react-router-dom'
+
+import useFetch from '../../hooks/useFetch'
+import DetailsBanner from './detailsBanner/DetailsBanner'
 
 const Details = () => {
+  const { mediaType, id } = useParams();
+    const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+    const { data: credits, loading: creditsLoading } = useFetch(
+        `/${mediaType}/${id}/credits`
+    );
+
   return (
-    <div>Details</div>
+    <div>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+    </div>
   )
 }
 
