9bebee92b5fd393dd2735f1a389dd0102f18e8a1
KOTSUPYR MAKSYM <maksym.kotsupyr@gmail.com>
2023-07-17 18:47:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ function App() {
         <Route path="/:mediaType/:id" element={<Details />} />
         <Route path="/search/:query" element={<SearchResult />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
-        <Route path="*" element={<PageNotFound />} />
+        {/* <Route path="*" element={<PageNotFound />} /> */}
       </Routes>
       <Footer />
     </BrowserRouter>
