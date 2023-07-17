2547d7b1957e5ed059d19e4903ad7b15d62741ac
KOTSUPYR MAKSYM <maksym.kotsupyr@gmail.com>
2023-07-17 18:00:13 +0300

    404  page

--- src/pages/404/pageNotFound.jsx
+++ src/pages/404/pageNotFound.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './style.scss'
+import React from "react";
 
-const pageNotFound = () => {
-  return (
-    <div>pageNotFound</div>
-  )
-}
+import "./style.scss";
 
-export default pageNotFound
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
