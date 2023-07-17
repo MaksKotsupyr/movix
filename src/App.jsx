f2a3d4bf5f1734bbc95dacda7faeaf71b7efdc41
KOTSUPYR MAKSYM <maksym.kotsupyr@gmail.com>
2023-07-17 18:54:10 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,73 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { fetchDataFromApi } from './utils/api'
-import { useSelector, useDispatch } from 'react-redux';
-import { getApiConfiguration, getGenres } from './store/homeSlice';
-import { BrowserRouter, Route, Routes  } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
 
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Home from './pages/home/Home';
-import Details from './pages/details/Details';
-import SearchResult from './pages/searchResult/SearchResult';
-import Explore from './pages/explore/Explore';
-import PageNotFound  from './pages/404/PageNotFound';
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
-  const dispatch = useDispatch();
-  const {url} = useSelector((state) => state.home);
+    const dispatch = useDispatch();
+    const { url } = useSelector((state) => state.home);
+    console.log(url);
 
-  useEffect(() => {
-    fetchApiConfig();
-    genresCall();
-  }, []);
+    useEffect(() => {
+        fetchApiConfig();
+        genresCall();
+    }, []);
 
-  const fetchApiConfig = () => {
-    fetchDataFromApi('/configuration')
-      .then((data) => {
+    const fetchApiConfig = () => {
+        fetchDataFromApi("/configuration").then((res) => {
+            console.log(res);
 
-        const url = {
-          backrop: data.images.secure_base_url + 'original', 
-          poster: data.images.secure_base_url + 'original', 
-          profile: data.images.secure_base_url + 'original', 
-        }
+            const url = {
+                backrop: res.images.secure_base_url + "original",
+                poster: res.images.secure_base_url + "original",
+                profile: res.images.secure_base_url + "original",
+            };
 
-        dispatch(getApiConfiguration(url));
-      })
-      .catch((error) => {
-        console.log(error) 
-      })
-  }
+            dispatch(getApiConfiguration(url));
+        });
+    };
 
-  const genresCall = async () => {
-    let promises = [];
-    let endPoints = ['tv', 'movie'];
-    let allGenres = {};
+    const genresCall = async () => {
+        let promises = [];
+        let endPoints = ["tv", "movie"];
+        let allGenres = {};
 
-    endPoints.forEach((url) => {
-      promises.push(fetchDataFromApi(`/genre/${url}/list`));
-    })
+        endPoints.forEach((url) => {
+            promises.push(fetchDataFromApi(`/genre/${url}/list`));
+        });
 
-    const data = await Promise.all(promises);
-    data.map(({genres}) => {
-      return genres.map((genre) => {
-        allGenres[genre.id] = genre;
-      })
-    })
-    
-    dispatch(getGenres(allGenres));
-  }
+        const data = await Promise.all(promises);
+        console.log(data);
+        data.map(({ genres }) => {
+            return genres.map((item) => (allGenres[item.id] = item));
+        });
 
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/:mediaType/:id" element={<Details />} />
-        <Route path="/search/:query" element={<SearchResult />} />
-        <Route path="/explore/:mediaType" element={<Explore />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  )
+        dispatch(getGenres(allGenres));
+    };
+
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
