e361ce60c760d142cc8950efe4b8c37f8bcb6027
KOTSUPYR MAKSYM <maksym.kotsupyr@gmail.com>
2023-07-17 18:28:54 +0300

    Revert "404  page"
    This reverts commit 2547d7b1957e5ed059d19e4903ad7b15d62741ac.

--- src/pages/404/pageNotFound.jsx
+++ src/pages/404/pageNotFound.jsx
@@ -1,18 +1,10 @@
-import React from "react";
+import React from 'react'
+import './style.scss'
 
-import "./style.scss";
+const pageNotFound = () => {
+  return (
+    <div>pageNotFound</div>
+  )
+}
 
-import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
-
-const PageNotFound = () => {
-    return (
-        <div className="pageNotFound">
-            <ContentWrapper>
-                <span className="bigText">404</span>
-                <span className="smallText">Page not found!</span>
-            </ContentWrapper>
-        </div>
-    );
-};
-
-export default PageNotFound;
+export default pageNotFound
