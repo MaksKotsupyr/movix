0461c9ebc60f4ccecfed3536b452b1e36ea2f84c
KOTSUPYR MAKSYM <maksym.kotsupyr@gmail.com>
2023-07-17 18:55:59 +0300

    remove error page

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
-import PageNotFound from "./pages/404/PageNotFound";
+// import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
     const dispatch = useDispatch();
@@ -63,7 +63,7 @@ function App() {
                 <Route path="/:mediaType/:id" element={<Details />} />
                 <Route path="/search/:query" element={<SearchResult />} />
                 <Route path="/explore/:mediaType" element={<Explore />} />
-                <Route path="*" element={<PageNotFound />} />
+                {/* <Route path="*" element={<PageNotFound />} /> */}
             </Routes>
             <Footer />
         </BrowserRouter>
